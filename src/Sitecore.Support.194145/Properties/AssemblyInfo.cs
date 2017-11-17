f435d543d3ef968a87ebdea11c41c88b886f70bb
Pavel <piv@sitecore.net>
2017-11-17 16:57:35 +0200

    Fix issue by adding ignore rule

---
+++ src/Sitecore.Support.194145/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.194145")]
+[assembly: AssemblyProduct("Sitecore.Support.194145")]
+[assembly: ComVisible(false)]
